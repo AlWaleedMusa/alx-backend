8c5a2a86bd3f39c1cd72904c46e9236171057663
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-22 03:29:38 +0300

    add: task 4 done

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+MRU Cache Replacement Implementation Class.
+
+This module provides an MRU (Most Recently Used) caching mechanism.
+"""
+from threading import RLock
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache class that inherits from BaseCaching.
+
+    This class provides methods to add and retrieve
+    items from a cache using MRU replacement policy.
+    """
+
+    def __init__(self):
+        """
+        Initialize the MRUCache instance.
+        """
+        super().__init__()
+        self.__keys = []
+        self.__rlock = RLock()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache.
+
+        Args:
+            key (str): The key under which the item is stored.
+            item (Any): The item to be stored in the cache.
+        """
+        if key is not None and item is not None:
+            keyOut = self._balance(key)
+            with self.__rlock:
+                self.cache_data.update({key: item})
+            if keyOut is not None:
+                print("DISCARD: {}".format(keyOut))
+
+    def get(self, key):
+        """
+        Get an item by key.
+
+        Args:
+            key (str): The key of the item to retrieve.
+
+        Returns:
+            Any: The item stored in the cache,
+            or None if the key does not exist.
+        """
+        with self.__rlock:
+            value = self.cache_data.get(key, None)
+            if key in self.__keys:
+                self._balance(key)
+        return value
+
+    def _balance(self, keyIn):
+        """
+        Balance the cache by removing the most recently
+        used item if the cache is full.
+
+        Args:
+            keyIn (str): The key of the new item being added.
+
+        Returns:
+            str: The key of the item that was removed,
+            or None if no item was removed.
+        """
+        keyOut = None
+        with self.__rlock:
+            keysLength = len(self.__keys)
+            if keyIn not in self.__keys:
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    keyOut = self.__keys.pop(keysLength - 1)
+                    self.cache_data.pop(keyOut)
+            else:
+                self.__keys.remove(keyIn)
+            self.__keys.insert(keysLength, keyIn)
+        return keyOut
